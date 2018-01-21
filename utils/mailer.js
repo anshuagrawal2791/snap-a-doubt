9ab0a22b3be7ada907fdbbf9fa11a37ebd99bdd3
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2018-01-21 21:13:51 +0530

    trying mailer

---
+++ utils/mailer.js
@@ -0,0 +1,31 @@
+const mailer = require('nodemailer');
+const configs = require('../config');
+module.exports.mail=(recipient,data,cb)=>{
+    var transporter = mailer.createTransport({
+        host:'smtp.mailtrap.io',
+        port:465,
+        secure:true,
+        auth: {
+          user: "69271bffcbc1a9",
+          pass: "754e9bb7253a62"
+        },
+        proxy:'http://172.16.2.30:8080'
+      });
+      var mailOptions = {
+        from: 'snap-a-doubt@gmail.com',
+        to: recipient,
+        subject: 'New Doubt',
+        text: data
+      };
+
+      transporter.sendMail(mailOptions, function(error, info){
+        console.log(error);
+        console.log(info)
+        if (error) {
+          return cb(error,null);
+        } else {
+          return cb(null,info);
+        }
+      });
+
+}
