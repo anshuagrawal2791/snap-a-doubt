e22954f3ddbbd9d25119a07993b83dfbaa3cd665
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2018-02-03 22:40:31 +0530

    bug fix

--- utils/sendNotif.js
+++ utils/sendNotif.js
@@ -22,37 +22,6 @@ exports.send = (topic, message, to, metadata, callback) => {
 		if(err)
 			return callback(err);
 		callback(null,resp);
-	})
-	// let options = {
-	// 	method: 'POST',
-	// 	url: 'https://gcm-http.googleapis.com/gcm/send',
-	// 	headers: {
-	// 		'Content-Type':'application/json',
-	// 		'Authorization': 'key='+configs.GCM_API_KEY
-	// 	},
-	// 	body : JSON.stringify({ "data": {
-
-	// 		"topic": topic,
-	// 		"message": message,
-	// 		"metadata": metadata
-	// 	},
-	// 	"to" : to
-	// })
-	// };
-
-	// request.post(options,(error,response,body)=>{
-	// 	console.log(error);
-	// 	console.log(response.statusCode);
-	// 	// console.log(response);
-
-	// 	if (!error && response.statusCode == 200) {
-	// 		callback(true); // Show the HTML for the Google homepage. 
-	// 	}	
-	// 	else
-	// 		callback(false);
-
-
-	// });
-	//callback(null, 'notif sent');
+	});
 
 };
