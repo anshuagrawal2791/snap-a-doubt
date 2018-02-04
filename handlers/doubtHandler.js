5636b6f78c30164103c71fdcc68a3aa7e4c001c0
Anshuman Agrawal <anshuagrawal2791@gmail.com>
2018-02-04 19:49:38 +0530

    fixed sorting while doubt allocation to tutors

--- handlers/doubtHandler.js
+++ handlers/doubtHandler.js
@@ -71,7 +71,7 @@ findAndSendToTutor = (doubt, cb) => {
     received_today: { $lt: configs.app.dailySolutionLimit },
     level: { $gt: 0 },
     available: true
-  }).sort('solved_today').sort('level').exec((err, tutor) => {
+  }).sort({'solved_today':1,'level':1}).exec((err, tutor) => {
     if (err) {
       return cb(err, null);
     }
